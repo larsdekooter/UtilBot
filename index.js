78b572c762f42a660f71c32908c2d52fb8934fec
Lars de Kooter <de.kooter.lars@gmail.com>
2022-10-01 20:56:53 +0200

    File structure

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.static("Public"));
 
 app.get("/", (req, res) =>
-  res.sendFile("index.html", { root: path.join(__dirname, "Public") })
+  res.sendFile("index.html", { root: path.join(__dirname, "Public", "HTML") })
 );
 app.post(
   "/inter",
