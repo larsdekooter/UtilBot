b3aba9f711d9a4887bad84a934d5d2611de160f0
Lars de Kooter <de.kooter.lars@gmail.com>
2022-10-07 19:14:14 +0200

    ROUTEs

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const __dirname = dirname(__filename);
 const app = express();
 app.use(express.static("Public"));
 
+let route;
 const client = new Client(app, {
   clientPublicKey:
     "d8c09e3ffb1c254322b098b64801f519d5401b07feccc272954739fb81c6f49a",
@@ -20,7 +21,7 @@ app.get("/", (req, res) =>
 );
 
 client.on("interactionCreate", async (interaction) => {
-  throw new Error(`${client.latestCode}, ${client.latestResponseStatusCode}`);
+  throw new Error(`${client.latestCode}, ${client.route}`);
   if (!interaction.isChatInputCommand()) return;
   if (!client.isReady) return interaction.reply("Bro I am not readyf");
   if (interaction.commandName === "grav") {
@@ -28,7 +29,7 @@ client.on("interactionCreate", async (interaction) => {
   }
 });
 
-client.on("ready", async () => {});
+client.rest.on("response", (req, res) => (route = req.route));
 
 app.listen(3000, () => console.log("seeya"));
 client.loginWithoutFetching(process.env.token);
