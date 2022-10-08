c248f9d737b1390aea7d62ee186a5e2e42d7764d
Lars de Kooter <de.kooter.lars@gmail.com>
2022-10-08 12:45:49 +0200

    fix error

--- index.js
+++ index.js
@@ -82,11 +82,10 @@ app.post(
       req.body.guild = await client.guilds.fetch(req.body.guild_id);
       req.body.member = new Member(req.body.member, req.body.guild);
     }
-    if (req.body.isChatInputCommand())
-      req.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: { content: (Date.now() - req.body.createdTimestamp).toString() },
-      });
+    req.send({
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: { content: (Date.now() - req.body.createdTimestamp).toString() },
+    });
   }
 );
 
