7d006cccaddecb1d9d22466717717df8efbd72be
Lars de Kooter <de.kooter.lars@gmail.com>
2022-10-08 12:44:48 +0200

    COmments yeah

--- index.js
+++ index.js
@@ -36,56 +36,56 @@ app.post(
     "d8c09e3ffb1c254322b098b64801f519d5401b07feccc272954739fb81c6f49a"
   ),
   async (req, res) => {
-    let interaction;
-    switch (req.body.type) {
-      case InteractionType.ApplicationCommand: {
-        if (req.body.data.type === ApplicationCommandType.ChatInput) {
-          interaction = new ChatInputCommandInteraction(res, req.body, client);
-        } else if (req.body.data.type === ApplicationCommandType.Message) {
-          interaction = new MessageContextMenuCommandInteraction(
-            res,
-            req.body,
-            client
-          );
-        } else {
-          interaction = new UserContextMenuCommandInteraction(
-            res,
-            req.body,
-            client
-          );
-        }
-        break;
-      }
-      case InteractionType.ApplicationCommandAutocomplete: {
-        interaction = new AutocompleteInteraction(res, req.body, client);
-        break;
-      }
-      case InteractionType.MessageComponent: {
-        if (req.body.data.component_type === ComponentType.SelectMenu) {
-          interaction = new SelectMenuInteraction(res, req.body, client);
-          break;
-        } else if (req.body.data.component_type === ComponentType.Button) {
-          interaction = new ButtonInteraction(res, req.body, client);
-          break;
-        }
-      }
-      case InteractionType.ModalSubmit: {
-        interaction = new ModalSubmitInteraction(res, req.body, client);
-        break;
-      }
-      default: {
-        interaction = new Interaction(res, req.body, client);
-        break;
-      }
+    // switch (req.body.type) {
+    //   case InteractionType.ApplicationCommand: {
+    //     if (req.body.data.type === ApplicationCommandType.ChatInput) {
+    //       interaction = new ChatInputCommandInteraction(res, req.body, client);
+    //     } else if (req.body.data.type === ApplicationCommandType.Message) {
+    //       interaction = new MessageContextMenuCommandInteraction(
+    //         res,
+    //         req.body,
+    //         client
+    //       );
+    //     } else {
+    //       interaction = new UserContextMenuCommandInteraction(
+    //         res,
+    //         req.body,
+    //         client
+    //       );
+    //     }
+    //     break;
+    //   }
+    //   case InteractionType.ApplicationCommandAutocomplete: {
+    //     interaction = new AutocompleteInteraction(res, req.body, client);
+    //     break;
+    //   }
+    //   case InteractionType.MessageComponent: {
+    //     if (req.body.data.component_type === ComponentType.SelectMenu) {
+    //       interaction = new SelectMenuInteraction(res, req.body, client);
+    //       break;
+    //     } else if (req.body.data.component_type === ComponentType.Button) {
+    //       interaction = new ButtonInteraction(res, req.body, client);
+    //       break;
+    //     }
+    //   }
+    //   case InteractionType.ModalSubmit: {
+    //     interaction = new ModalSubmitInteraction(res, req.body, client);
+    //     break;
+    //   }
+    //   default: {
+    //     interaction = new Interaction(res, req.body, client);
+    //     break;
+    //   }
+    // }
+    await client.channels.fetch(req.body.channel_id);
+    if (req.body.guild_id) {
+      req.body.guild = await client.guilds.fetch(req.body.guild_id);
+      req.body.member = new Member(req.body.member, req.body.guild);
     }
-    await client.channels.fetch(interaction.channelId);
-    if (interaction.guildId) {
-      interaction.guild = await client.guilds.fetch(interaction.guildId);
-      interaction.member = new Member(interaction._member, interaction.guild);
-    }
-    if (interaction.isChatInputCommand())
-      interaction.reply({
-        content: (Date.now() - interaction.createdTimestamp).toString(),
+    if (req.body.isChatInputCommand())
+      req.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: { content: (Date.now() - req.body.createdTimestamp).toString() },
       });
   }
 );
