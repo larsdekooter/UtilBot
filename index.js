dbb77c76a677248938b3403cfe6e39dae89c5d87
Lars de Kooter <de.kooter.lars@gmail.com>
2022-10-08 12:42:28 +0200

    Entire client function copy

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
 import path, { dirname } from "path";
 import {
+  ApplicationCommandType,
+  AutocompleteInteraction,
+  ButtonInteraction,
   ChatInputCommandInteraction,
   Client,
+  Interaction,
   InteractionResponseType,
+  InteractionType,
   Member,
+  MessageContextMenuCommandInteraction,
+  ModalSubmitInteraction,
+  SelectMenuInteraction,
+  UserContextMenuCommandInteraction,
   verifyKeyMiddleware,
 } from "kooterdiscordstructures";
 import { fileURLToPath } from "url";
@@ -27,16 +36,57 @@ app.post(
     "d8c09e3ffb1c254322b098b64801f519d5401b07feccc272954739fb81c6f49a"
   ),
   async (req, res) => {
-    const interaction = new ChatInputCommandInteraction(res, req.body, client);
+    let interaction;
+    switch (req.body.type) {
+      case InteractionType.ApplicationCommand: {
+        if (req.body.data.type === ApplicationCommandType.ChatInput) {
+          interaction = new ChatInputCommandInteraction(res, req.body, client);
+        } else if (req.body.data.type === ApplicationCommandType.Message) {
+          interaction = new MessageContextMenuCommandInteraction(
+            res,
+            req.body,
+            client
+          );
+        } else {
+          interaction = new UserContextMenuCommandInteraction(
+            res,
+            req.body,
+            client
+          );
+        }
+        break;
+      }
+      case InteractionType.ApplicationCommandAutocomplete: {
+        interaction = new AutocompleteInteraction(res, req.body, client);
+        break;
+      }
+      case InteractionType.MessageComponent: {
+        if (req.body.data.component_type === ComponentType.SelectMenu) {
+          interaction = new SelectMenuInteraction(res, req.body, client);
+          break;
+        } else if (req.body.data.component_type === ComponentType.Button) {
+          interaction = new ButtonInteraction(res, req.body, client);
+          break;
+        }
+      }
+      case InteractionType.ModalSubmit: {
+        interaction = new ModalSubmitInteraction(res, req.body, client);
+        break;
+      }
+      default: {
+        interaction = new Interaction(res, req.body, client);
+        break;
+      }
+    }
     await client.channels.fetch(interaction.channelId);
     if (interaction.guildId) {
       interaction.guild = await client.guilds.fetch(interaction.guildId);
       interaction.member = new Member(interaction._member, interaction.guild);
     }
-    res.send({
-      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-      data: { content: (Date.now() - interaction.createdTimestamp).toString() },
-    });
+    if (interaction.isChatInputCommand())
+      interaction.reply({
+        content: (Date.now() - interaction.createdTimestamp).toString(),
+      });
   }
 );
 
