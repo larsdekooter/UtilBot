b4c37cc8042db659c2db57716939250232d3b193
Lars de Kooter <de.kooter.lars@gmail.com>
2022-10-08 12:30:21 +0200

    raw listener

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import path, { dirname } from "path";
-import { Client } from "kooterdiscordstructures";
+import {
+  Client,
+  InteractionResponseType,
+  verifyKeyMiddleware,
+} from "kooterdiscordstructures";
 import { fileURLToPath } from "url";
 import "dotenv/config";
 
@@ -9,29 +13,21 @@ const __dirname = dirname(__filename);
 const app = express();
 app.use(express.static("Public"));
 
-let route;
-const client = new Client(app, {
-  clientPublicKey:
-    "d8c09e3ffb1c254322b098b64801f519d5401b07feccc272954739fb81c6f49a",
-  route: "/inter",
-});
-
 app.get("/", (req, res) =>
   res.sendFile("index.html", { root: path.join(__dirname, "Public") })
 );
 
-client.on("interactionCreate", async (interaction) => {
-  throw new Error(`${client.latestCode}, ${client.route}`);
-  if (!interaction.isChatInputCommand()) return;
-  if (!client.isReady) return interaction.reply("Bro I am not readyf");
-  if (interaction.commandName === "grav") {
-    await interaction.reply("Cleared");
+app.post(
+  "/inter",
+  verifyKeyMiddleware(
+    "d8c09e3ffb1c254322b098b64801f519d5401b07feccc272954739fb81c6f49a"
+  ),
+  (req, res) => {
+    res.send({
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: { content: "fakka" },
+    });
   }
-});
-
-client.rest.on("response", (req, res) => {
-  throw new Error("Response");
-});
+);
 
 app.listen(3000, () => console.log("seeya"));
-client.loginWithoutFetching(process.env.token);
